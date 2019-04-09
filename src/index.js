c265df4e40fcef65300f27ddc4cd1f24857064c6
OussamaAlouat <al317257@uji.es>
2019-04-09 07:22:50 +0200

    Added two comments more, manually

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import faker from 'faker'; //TODO INSTALL THIS DEPENDENCY
 
 const App = () => {
   return (
   <div className="ui ontainer comments">
     <div className="comment">
       <a href="/" className="avatar">
-        <img className="avatar" />
+        <img alt="avatar" />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00PM</span>
+        </div>
+        <div className="text">Nice Blog post!</div>
+      </div>
+    </div>
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00PM</span>
+        </div>
+        <div className="text">Nice Blog post!</div>
+      </div>
+    </div>
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" />
       </a>
       <div className="content">
         <a href="/" className="author">
